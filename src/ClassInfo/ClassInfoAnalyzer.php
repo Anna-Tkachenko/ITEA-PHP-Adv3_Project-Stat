a10218f983b4e311823cf4f7662a00d94347756b
Anna Tkachenko <tkachenko.anna835@gmail.com>
2018-12-12 21:42:40 +0200

    Add ClassInfoCommand

---
+++ src/ClassInfo/ClassInfoAnalyzer.php
@@ -0,0 +1,124 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tkachenko
+ * Date: 12/12/18
+ * Time: 9:36 PM
+ */
+
+namespace App\ClassInfo;
+
+use Symfony\Component\Finder\Finder;
+
+/**
+ * Analyzer that provides information about classes properties and methods.
+ *
+ * @author Anna Tkachenko <tkachenko.anna835@gmail.com>
+ */
+final class ClassInfoAnalyzer
+{
+    private $rootDir;
+    private $rootNamespace;
+    private $info_storage = [];
+
+    public function __construct(string $rootDir, string $rootNamespace)
+    {
+        $this->rootDir = $rootDir;
+        $this->rootNamespace = $rootNamespace;
+    }
+
+    /**
+     * Analyzes class by name.
+     *
+     * @param string $name Full name of class.
+     * @return array Information about class properties and methods.
+     */
+    public function analyze(string $name): array
+    {
+        $finder = new Finder();
+        $finder
+            ->in($this->rootDir)
+            ->files()
+            ->name('/^[A-Z].+\.php$/')
+        ;
+
+        foreach ($finder as $file) {
+            $path = $file->getRelativePathname();
+
+            if (strrpos($path, '/')) {
+                $temp_array = explode('/', $path);
+                $new_path = $temp_array[1];
+                $new_path = rtrim($new_path, '.php');
+            } else {
+                $new_path = rtrim($path, '.php');
+            }
+
+            if ($new_path === $name) {
+                $fullClassName = $this->rootNamespace
+                    . '\\'
+                    . \str_replace('/', '\\', \rtrim($path, '.php'));
+
+
+                try {
+                    $reflection = new \ReflectionClass($fullClassName);
+                } catch (\ReflectionException $e) {
+                }
+
+                if ($reflection->isAbstract()) {
+                    $this->info_storage['class-type'] = 'Abstract';
+                } elseif ($reflection->isFinal()) {
+                    $this->info_storage['class-type'] = 'Final';
+                } else {
+                    $this->info_storage['class-type'] = 'Sample';
+                }
+
+                $this->info_storage['public_prop'] = count($reflection->getProperties(\ReflectionProperty::IS_PUBLIC));
+
+                $this->info_storage['protected_prop'] = count($reflection->getProperties(\ReflectionProperty::IS_PROTECTED));
+
+                $this->info_storage['private_prop'] = count($reflection->getProperties(\ReflectionProperty::IS_PRIVATE));
+
+                $this->info_storage['public_static_prop'] = 0;
+                $this->info_storage['protected_static_prop'] = 0;
+                $this->info_storage['private_static_prop'] = 0;
+
+                $static_properties = $reflection->getProperties(\ReflectionProperty::IS_STATIC);
+
+                foreach ($static_properties as $static_property) {
+                    if ($static_property->isPublic()) {
+                        $this->info_storage['public_static_prop']++;
+                    } elseif ($static_property->isProtected()) {
+                        $this->info_storage['protected_static_prop']++;
+                    } elseif ($static_property->isPrivate()) {
+                        $this->info_storage['private_static_prop']++;
+                    }
+                }
+
+                $this->info_storage['public_methods'] = count($reflection->getMethods(\ReflectionMethod::IS_PUBLIC));
+
+                $this->info_storage['protected_methods'] = count($reflection->getMethods(\ReflectionMethod::IS_PROTECTED));
+
+                $this->info_storage['private_methods'] = count($reflection->getMethods(\ReflectionMethod::IS_PRIVATE));
+
+                $this->info_storage['public_static_methods'] = 0;
+                $this->info_storage['protected_static_methods'] = 0;
+                $this->info_storage['private_static_methods'] = 0;
+
+                $static_methods = $reflection->getMethods(\ReflectionMethod::IS_STATIC);
+
+                foreach ($static_methods as $static_method) {
+                    if ($static_method->isPublic()) {
+                        $this->info_storage['public_static_methods']++;
+                    } elseif ($static_method->isProtected()) {
+                        $this->info_storage['protected_static_methods']++;
+                    } elseif ($static_method->isPrivate()) {
+                        $this->info_storage['private_static_methods']++;
+                    }
+                }
+            }
+        }
+
+
+        return $this->info_storage;
+    }
+}
---
+++ src/ClassInfoCommand.php
@@ -0,0 +1,98 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tkachenko
+ * Date: 12/12/18
+ * Time: 9:36 PM
+ */
+
+namespace App;
+
+use App\ClassInfo\ClassInfoAnalyzer;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Command for getting information about Class
+ * (types and amount of properties and methods).
+ *
+ * Example of usage
+ * ./bin/console stat:class-info ClassStatisticCommand
+ *
+ * @author Anna Tkachenko <tkachenko.anna835@gmail.com>
+ */
+final class ClassInfoCommand extends Command
+{
+    private $analyzer;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function __construct(ClassInfoAnalyzer $analyzer, string $name = null)
+    {
+        $this->analyzer = $analyzer;
+
+        parent::__construct($name);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('stat:class-info')
+            ->setDescription('Shows information about class properties and methods')
+            ->addArgument(
+                'class-name',
+                InputArgument::REQUIRED,
+                'Full name of needed class'
+            )
+        ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $name = $input->getArgument('class-name');
+
+        $class_info = $this->analyzer->analyze($name);
+
+        if (!($class_info == null)) {
+            $output->writeln(
+                \sprintf(
+                    'Class: %s' . \PHP_EOL .
+                    'Properties: ' . \PHP_EOL . 'public: %d (%d static)' . \PHP_EOL .
+                    'protected: %d (%d static)' . \PHP_EOL .
+                    'private: %d (%d static)' . \PHP_EOL .
+
+                    'Methods: ' . \PHP_EOL . 'public: %d (%d static)' . \PHP_EOL .
+                    'protected: %d (%d static)' . \PHP_EOL .
+                    'private: %d (%d static)'
+
+                    ,$class_info['class-type'],
+                    $class_info['public_prop'],
+                    $class_info['public_static_prop'],
+                    $class_info['protected_prop'],
+                    $class_info['protected_static_prop'],
+                    $class_info['private_prop'],
+                    $class_info['private_static_prop'],
+                    $class_info['public_methods'],
+                    $class_info['public_static_methods'],
+                    $class_info['protected_methods'],
+                    $class_info['protected_static_methods'],
+                    $class_info['private_methods'],
+                    $class_info['private_static_methods']
+                )
+            );
+        } else {
+            $output->writeln(
+                \sprintf('Undefined class name.')
+            );
+        }
+    }
+}
